9ceb96c03032834ca39616e0d9ff96bd1441aee8
Rajkumar17 <rajkumarm1703@gmail.com>
2024-05-27 14:04:16 +0530

    updated

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
-
-app=Flask(__name__)
-
-
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "wek"
+    return 'Hello, Flask!'
 
+@app.route('/about')
+def about():
+    return 'About Page'
 
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
+t
